a2c834cda3c50456772741b76bf7fd4fcccc8d64
Shogo Nakano <tsvx7433@gmail.com>
2021-06-17 09:07:02 +0900

    今エラーが発生していて、どう直したらよいのかわかっていないのだが、スラックで質問

--- src/index.js
+++ src/index.js
@@ -10,60 +10,20 @@ const Square = (props) => {
   );
 }
 
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  const handleClick = (i) => {
-    const squaresval = squares.slice();
-    squaresval[i] = xIsNext ? 'x' : 'o';
-    setSquares(() => squaresval);
-    setXIsNext(() => !xIsNext)
-  }
-
+const Board = (props) => {
+  
   const renderSquare = (i) => {
     return( 
-      <Square 
-        value={squares[i]}
-        onClick={() => handleClick(i)}
+      <Square
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)}
       />
     );
   }
   
-  const calculateWinner = (squares) => {
-    // この線上に同じ値が並んでいたら勝ち
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
-  // const status = 'Next player: ' + (xIsNext ? 'x' : 'o');
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'x' : 'o');
-  }
-
-
   return (
     <div>
-      <div className="status">{status}</div>
+      <div className="status">{props.status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -83,20 +43,71 @@ const Board = () => {
   );
 }
 
+const calculateWinner = (squares) => {
+  // この線上に同じ値が並んでいたら勝ち
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 
 const Game = () => {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
+  // classのconstructorに当たる処理
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState(squares);
+  const [xIsNext, setXIsNext] = useState(true);
+  
+  const current = history[history.length-1];
+
+  const handleClick = (i) => {
+    const squaresval = current.squares.slice();
+    if (calculateWinner(squaresval) || squaresval[i]) {
+      return;
+    }
+    squaresval[i] = xIsNext ? 'x' : 'o';
+    setSquares(() => squaresval);
+    setHistory(() => history.concat(squares));
+    setXIsNext(() => !xIsNext);
   }
+  
+  // 判定処理に基づき、表示するwinnerを出しわける処理
+  const winner = calculateWinner(current.squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'x' : 'o');
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board 
+          squares={current.squares}
+          onClick={(i) => handleClick(i)}
+        />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+}
 
 // ========================================
 
@@ -104,3 +115,4 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
