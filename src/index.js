e6d54d4cc11d8a404740f71f081fca4ca578d1be
Shogo Nakano <tsvx7433@gmail.com>
2021-06-18 09:23:26 +0900

    timeTravelを使って戻ることはできるようになったのだが、戻った後にどこかをクリックすると、戻る直前のhistoryが使われてしまう挙動になっている。バグフィックスする必要あり。

--- src/index.js
+++ src/index.js
@@ -51,15 +51,38 @@ const Game = (props) => {
   // const [squares, setSquares] = useState(Array(9).fill(null));
   const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
   const [xIsNext, setXIsNext] = useState(true);
+  const [stepNumber, setStepNumber] = useState(0);
+  const rendercurrent = history[stepNumber];
+
+
+  const jumpTo = (step) => {
+    setStepNumber(() => step);
+    setXIsNext(() => (step%2) === 0);
+  };
+
+  const moves = history.map((step, move) => {
+    const desc = move ?
+      'Go to move #' + move :
+      'Go to move start';
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
   
-  const current = history[history.length-1];
+  
   const handleClick = (i) => {
+    const current = history[history.length - 1];
+    const timeTraveledHistory = history.slice(0, stepNumber+1);
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = xIsNext ? 'x' : 'o';
     setHistory(() => history.concat([{squares: squares}]));
+    setStepNumber(() => timeTraveledHistory.length);
     setXIsNext(() => !xIsNext);
   }
   // 外にあった関数を中に入れた
@@ -85,7 +108,7 @@ const Game = (props) => {
   };
   
   // 判定処理に基づき、表示するwinnerを出しわける処理
-  const winner = calculateWinner(current.squares);
+  const winner = calculateWinner(rendercurrent.squares);
   let status;
   if (winner) {
     status = 'Winner: ' + winner;
@@ -97,13 +120,13 @@ const Game = (props) => {
     <div className="game">
       <div className="game-board">
         <Board 
-          squares={current.squares}
+          squares={rendercurrent.squares}
           onClick={(i) => handleClick(i)}
         />
       </div>
       <div className="game-info">
         <div>{status}</div>
-        <ol>{/* TODO */}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
